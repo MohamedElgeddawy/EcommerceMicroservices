2e303b60972622668d586b158e344273d1b420cc
Mohamed Eslam <mohamed2000esla@gmail.com>
2025-03-03 16:45:47 +0200

    Added Product API feature

---
+++ DenoEcommerceProductApiSolution/ProductApi.Infrastructure/DependencyInjection/ServiceContainer.cs
@@ -0,0 +1,34 @@
+﻿using eCommerce.SharedLibrary.DependencyInjection;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using ProductApi.Application.Interfaces;
+using ProductApi.Infrastructure.Data;
+using ProductApi.Infrastructure.Repositories;
+
+
+namespace ProductApi.Infrastructure.DependencyInjection
+{
+    public static class ServiceContainer
+    {
+        public static IServiceCollection AddInfrastructureService(this IServiceCollection services, IConfiguration configuration)
+        {
+            // Add database connectivity
+            // Add authentication schemes
+            SharedServiceContainer.AddSharedServices<ProductDbContext>(services, configuration, configuration["MySerilog:FileName"]!);
+
+            // Create Dependency Injection 
+            services.AddScoped<IProduct, ProductRepository>();
+            return services;
+        }
+
+        public static IApplicationBuilder UseInfrastructurePolicy(this IApplicationBuilder app)
+        {
+            // Add middleware
+            // Global Exception : handle all exceptions
+            // ListenToOnlyApiGateway : block all outsiders API calls
+            SharedServiceContainer.UseShardPolicies(app);
+            return app;
+        }
+    }
+}
