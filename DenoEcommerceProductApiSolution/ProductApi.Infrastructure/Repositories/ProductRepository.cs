ccbef265ed6a7a0b9528b31a6758104680d271a6
Mohamed Eslam <mohamed2000esla@gmail.com>
2025-03-07 23:09:24 +0200

    Added Product API

---
+++ DenoEcommerceProductApiSolution/ProductApi.Infrastructure/Repositories/ProductRepository.cs
@@ -0,0 +1,134 @@
+﻿using eCommerce.SharedLibrary.Logs;
+using eCommerce.SharedLibrary.Responses;
+using Microsoft.EntityFrameworkCore;
+using ProductApi.Application.Interfaces;
+using ProductApi.Domain.Entities;
+using ProductApi.Infrastructure.Data;
+using System.Linq.Expressions;
+
+
+namespace ProductApi.Infrastructure.Repositories
+{
+    public class ProductRepository(ProductDbContext context) : IProduct
+    {
+        public async Task<Response> CreateAsync(Product entity)
+        {
+            try
+            {
+                //check if product already exists
+                var getProduct = await GetByAsync(_ => _.Name!.Equals( entity.Name));
+                if (getProduct != null && !string.IsNullOrEmpty(getProduct.Name))
+                    return new Response(false, $"{entity.Name} already added");
+                
+                var currentEntity = context.Products.Add(entity).Entity;
+                await context.SaveChangesAsync();
+                if(currentEntity is not null && currentEntity.Id >0)
+                    return new Response(true, $"{entity.Name} created successfully");
+                else
+                    return new Response(false, $"{entity.Name}  creation failed");
+            }
+            catch (Exception ex)
+            {
+                // Log the original exception message
+                LogException.LogExceptions(ex);
+
+                //display scary-free message to the user
+                return new Response(false, "Product creation failed");
+            }
+        }
+
+        public async Task<Response> DeleteAsync(Product entity)
+        {
+            try
+            {
+                var product = await FindByIdAsync(entity.Id);
+                if(product is null)
+                    return new Response(false, $"{entity.Name} not found");
+
+                context.Products.Remove(product);
+                await context.SaveChangesAsync();
+                return new Response(true, $"{entity.Name} deleted successfully");
+            }
+            catch (Exception ex)
+            {
+                // Log the original exception message
+                LogException.LogExceptions(ex);
+
+                //display scary-free message to the user
+                return new Response(false, "Product deleting failed");
+            }
+        }
+
+        public async Task<Product> FindByIdAsync(int id)
+        {
+            try
+            {
+                var product = await context.Products.FindAsync(id);
+                return product is not null? product : null!;
+            }
+            catch (Exception ex)
+            {
+                // Log the original exception message
+                LogException.LogExceptions(ex);
+
+                //display scary-free message to the user
+                throw new Exception("Product retrieving failed");
+            }
+        }
+
+        public async Task<IEnumerable<Product>> GetAllAsync()
+        {
+            try
+            {
+                var products= await context.Products.AsNoTracking().ToListAsync();
+                return products is not null ? products : null!;
+            }
+            catch (Exception ex)
+            {
+                // Log the original exception message
+                LogException.LogExceptions(ex);
+
+                //display scary-free message to the user
+                throw new InvalidOperationException("Products retrieving failed");
+            }
+        }
+
+        public async Task<Product> GetByAsync(Expression<Func<Product, bool>> predicate)
+        {
+            try
+            {
+                var product = await context.Products.AsNoTracking().Where(predicate).FirstOrDefaultAsync();
+                return product is not null ? product : null!;
+            }
+            catch (Exception ex)
+            {
+                // Log the original exception message
+                LogException.LogExceptions(ex);
+                //display scary-free message to the user
+                throw new InvalidOperationException("Product retrieving failed");
+            }
+        }
+
+        public async Task<Response> UpdateAsync(Product entity)
+        {
+            try
+            {
+                var product = await FindByIdAsync(entity.Id);
+                if (product is null)
+                    return new Response(false, $"{entity.Name} not found");
+
+                context.Entry(product).State = EntityState.Detached;
+                context.Products.Update(entity);
+                context.SaveChanges();
+                return new Response(true, $"{entity.Name} updated successfully");
+            }
+            catch (Exception ex)
+            {
+                // Log the original exception message
+                LogException.LogExceptions(ex);
+                //display scary-free message to the user
+                return new Response(false, "Product updating failed");
+            }
+        }
+    }
+}
---
+++ DenoEcommerceProductApiSolution/ProductApi.Presentation/Controllers/ProductsController.cs
@@ -0,0 +1,77 @@
+﻿using eCommerce.SharedLibrary.Responses;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using ProductApi.Application.DTOs;
+using ProductApi.Application.DTOs.Conversions;
+using ProductApi.Application.Interfaces;
+
+namespace ProductApi.Presentation.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductsController(IProduct productInterface) : ControllerBase
+    {
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<ProductDTO>>> GetProducts()
+        {
+            // Get all products from repo
+            var products = await productInterface.GetAllAsync();
+            if(!products.Any())
+            return NotFound("No prducts detected in the database");
+
+            // Convert to DTO
+            var (_, list) = ProductConversion.FromEntity(null, products);
+            return list.Any() ? Ok(list) : NotFound("No products detected in the database");
+        }
+
+        [HttpGet("{id:int}")]
+        public async Task<ActionResult<ProductDTO>> GetProduct(int id)
+        {
+            // Get product by id
+            var product = await productInterface.FindByIdAsync(id);
+            if (product is null)
+                return NotFound("Product not found");
+            // Convert to DTO
+            var (_product, _) = ProductConversion.FromEntity(product, null);
+            return _product is not null ? Ok(_product) : NotFound("Product not found");
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<Response>> CreateProduct(ProductDTO product)
+        {
+            // check model state is all data annotations are valid
+            if (!ModelState.IsValid)
+                return BadRequest("Invalid model state");
+
+            // convet to entity
+            var getEntity = ProductConversion.ToEntity(product);
+            var response = await productInterface.CreateAsync(getEntity);
+            return response.Flag is true ? Ok(response) : BadRequest(response);
+        }
+
+
+        [HttpPut]
+        public async Task<ActionResult<Response>> UpdateProduct(ProductDTO product)
+        {
+            // check model state is all data annotations are valid
+            if (!ModelState.IsValid)
+                return BadRequest("Invalid model state");
+            // convet to entity
+            var getEntity = ProductConversion.ToEntity(product);
+            var response = await productInterface.UpdateAsync(getEntity);
+            return response.Flag is true ? Ok(response) : BadRequest(response);
+        }
+
+        [HttpDelete]
+        public async Task<ActionResult<Response>> DeleteProduct(ProductDTO product)
+        {
+            // convet to entity
+            var getEntity = ProductConversion.ToEntity(product);
+
+            var response = await productInterface.DeleteAsync(getEntity);
+            return response.Flag is true ? Ok(response) : BadRequest(response);
+        }
+
+    }
+}
